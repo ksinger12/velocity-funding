3736b243ec598cae9dde9feac4d91d72ec8a6fe1
ksinger12 <kylesinger@rogers.com>
2025-10-30 02:11:22 -0400

    adding setters to configs

--- src/main/java/com/funding/velocity/config/FundingLimitConfig.java
+++ src/main/java/com/funding/velocity/config/FundingLimitConfig.java
@@ -2,10 +2,12 @@ package com.funding.velocity.config;
 
 import java.util.Map;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties("funding.limits")
 public class FundingLimitConfig {
--- src/main/java/com/funding/velocity/config/SchemaConfig.java
+++ src/main/java/com/funding/velocity/config/SchemaConfig.java
@@ -3,10 +3,12 @@ package com.funding.velocity.config;
 import java.util.Map;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties("funding.schemas")
 public class SchemaConfig {
